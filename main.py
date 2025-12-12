6dd2f40dc2796f207c888478aab2374fccfcc660
Joonsun Auh <84356984+utjune@users.noreply.github.com>
2025-12-12 15:13:49 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import random as r
+
+def get_user_input(msg):
+    inputlist=[]
+    pass
+    return inputlist
+
+def link(inputlist1,inputlist2):
+    linkdict={}
+    pass
+    return linkdict
+
+list1=get_user_input(msg)
+list2=get_user_input(msg)
+
+for k,v in enumerate(link(list1,list2)):
+    print(f"{k}--{v}")
+
+
+def get_user_input():
+  inputlist = []
+  pass
+  return inputlist
